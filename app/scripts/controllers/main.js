433829129c3ecff88cdc4d96a54bf265f9158ca0
Nils Preusker <n.preusker@googlemail.com>
2014-12-19 15:30:39 +0100

    adding ui-bootstrap, mockup of video list + dialog

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,47 @@
  * Controller of the videoWallApp
  */
 angular.module('videoWallApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope, $modal, $log) {
+                $scope.videos = [
+                  {
+                    name: "Lake MP4",
+                    still: "http://www.jod.uk.com/media/1506/natural-world-hero.jpg?width=1280"
+                  },
+                  {
+                    name: "Tree MP4",
+                    still: "http://www.chakalacquer.dk/wp-content/uploads/2014/08/Autumn-Nature-Desktop.jpg"
+                  }
+                ];
+
+                $scope.open = function (video) {
+                  var modalInstance = $modal.open({
+                                                    templateUrl: 'views/VideoModal.html',
+                                                    controller: 'ModalCtrl',
+                                                    resolve: {
+                                                      video: function () {
+                                                        return video;
+                                                      }
+                                                    }
+                                                  });
+
+                  modalInstance.result.then(function (selectedItem) {
+                    $scope.selected = selectedItem;
+                  }, function () {
+                    $log.info('Modal dismissed at: ' + new Date());
+                  });
+                };
+
+              })
+  .controller('ModalCtrl', function ($scope, $modalInstance, video) {
+
+                $scope.video = video;
+
+                $scope.ok = function () {
+                  $modalInstance.close($scope.selected.item);
+                };
+
+                $scope.close = function () {
+                  $modalInstance.dismiss('cancel');
+                };
+
+              });
